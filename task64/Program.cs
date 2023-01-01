07a7e546b7d13cc627dcbf937ae74526e299678c
Dmitry <panfilovdima1@yandex.ru>
2023-01-01 13:48:04 +0300

    HW9 done

---
+++ task64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N, напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью реурсии.
+
+Console.WriteLine("Введите N");
+int n = int.Parse(Console.ReadLine()!);
+
+NaturalNumbersList(n);
+
+void NaturalNumbersList(int number)
+{
+    if (number == 1)
+    {
+        Console.Write("1 ");
+        return;
+    }
+    
+    Console.Write(number + ", ");    // Если сначала вызвать функцию  NaturalNumbersList(number - 1),
+    NaturalNumbersList(number - 1);  // а потом дать команду Console.Write(number + ", "), то числа выведутся в порядке возрастания.
+}
---
+++ task66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите M");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите N");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма элементов равна {SumNumbers(m, n)}");
+
+int SumNumbers(int firstNumber, int secondNumber)
+{
+    if (secondNumber < firstNumber)
+    {
+        int temp = secondNumber;
+        secondNumber = firstNumber;
+        firstNumber = temp;
+    }
+    
+    if (secondNumber == firstNumber)
+    {
+        return firstNumber;
+    }
+    else
+    {
+       return secondNumber + SumNumbers(firstNumber, secondNumber - 1); 
+    }
+}
